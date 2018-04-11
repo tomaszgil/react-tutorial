bf20e98d7c127fbc35081dfda2ce789c48c0de6d
Tomasz Gil <tomasz.gil@akai.org.pl>
2018-04-11 11:26:57 +0200

    Add collected indicator

--- src/Pokemon/Pokemon.js
+++ src/Pokemon/Pokemon.js
@@ -1,36 +1,57 @@
 import React, { Component } from 'react';
 import './Pokemon.css'
 
-const Pokemon = ({ name, id, type, img, collected, onPokemonCheck }) => {
-  const colors = {
-    'water': '#c5eefa',
-    'grass': '#d9f5cb',
-    'electric': '#faf4cf',
-    'fire': '#fde9e4',
-    'bug': '#efe1da',
-    'poison': '#eee9e8',
-    'normal': '#dfdffa',
-    'ground': '#dbcabc'
-  };
+class Pokemon extends Component {
+  constructor(props) {
+    super(props);
 
-  const style = {
-    background: colors[type]
-  };
+    this.state = {
+      collected: this.props.collected
+    };
 
-  return (
-    <li className="pokemon"
-        onClick={() => onPokemonCheck(id)}>
-      <div className="wrapper">
-        <div className="img-background" style={style} />
-        <img src={img} />
-      </div>
-      <div className="information">
-        <span className="name">{name}</span>
-        <span className="type">{type}</span>
-        <span className="id">{id}</span>
-      </div>
-    </li>
-  );
-};
+    this.handlePokeballClick = this.handlePokeballClick.bind(this);
+  }
+
+  handlePokeballClick() {
+    this.setState({
+      collected: !this.state.collected
+    });
+
+    this.props.onPokemonCheck(this.props.id);
+  }
+
+  render() {
+    // TODO global declaration of colors and types
+    const colors = {
+      'water': '#c5eefa',
+      'grass': '#d9f5cb',
+      'electric': '#faf4cf',
+      'fire': '#fde9e4',
+      'bug': '#efe1da',
+      'poison': '#eee9e8',
+      'normal': '#dfdffa',
+      'ground': '#dbcabc'
+    };
+
+    const style = {
+      background: colors[this.props.type]
+    };
+
+    return (
+      <li className={this.state.collected ? "pokemon collected" : "pokemon"}>
+        <div className="wrapper">
+          <div className="img-background" style={style}/>
+          <img src={this.props.img}/>
+          <a href="#" className="pokeball" onClick={this.handlePokeballClick}/>
+        </div>
+        <div className="information">
+          <span className="name">{this.props.name}</span>
+          <span className="type">{this.props.type}</span>
+          <span className="id">{this.props.id}</span>
+        </div>
+      </li>
+    );
+  }
+}
 
 export default Pokemon;
