e9c80ac353b90ed5c6e015bf5dfd5eec78acf923
Tomasz Gil <tomasz.gil@akai.org.pl>
2018-04-14 22:04:33 +0200

    Step 1 fix

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,10 @@
 import './App.css';
 
-// TODO Step 1:
-// TODO 1.1. Import React.
-// TODO 1.2. Create App class that extends React's Component class.
-// TODO 1.3. Component should have a field to store the data fetched from API.
-// TODO 1.4. Initialize the state of the component to have a single variable that indicates whether the data has been already fetched or not.
-// TODO 1.5. Create a function that will fetch a pokemon array from this URL:
+// TODO 1.3. Import React.
+// TODO 1.4. Create App class that extends React's Component class.
+// TODO 1.5. Component should have a field to store the data fetched from API.
+// TODO 1.6. Initialize the state of the component to have a single variable that indicates whether the data has been already fetched or not.
+// TODO 1.7. Create a function that will fetch a pokemon array from this URL:
 // TODO      https://api.mlab.com/api/1/databases/pokedex/collections/pokemons?apiKey=RZxUI6ohr3E8hmBGY6HDPlRWpXmVhzgh
 // TODO      Remember to bind this function properly, if you plan to use any component’s properties.
 
@@ -17,6 +16,6 @@ import './App.css';
 //     })
 //     .catch(err => console.error(err));
 
-// TODO 1.6. Invoke this function as soon as the component is mounted onto the page.
-// TODO 1.7. We want this component to render a div with a class of app, inside of which we want to have Logo component and Search component.
+// TODO 1.8. Invoke this function as soon as the component is mounted onto the page.
+// TODO 1.9. We want this component to render a div with a class of app, inside of which we want to have Logo component and Search component.
 // TODO      Remember to import these components.
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// TODO Step 1.
+// TODO 1.1. Import App component from src/App.
+// TODO 1.2. Render App component inside the element with id of "root".
+
 registerServiceWorker();
