5ed6bf925d0cc9e0ac46fb7a20ed1b1e859c88d7
Tomasz Gil <tomasz.gil@akai.org.pl>
2018-04-15 20:42:20 +0200

    Step 4 instructions

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -27,6 +27,17 @@ class Search extends Component {
     pokemon.collected = !pokemon.collected;
   }
 
+// TODO Step 4.
+// TODO 4.1. Add a new class property that will store our search criteria.
+// TODO      For now it can be a JavaScript object with a single property which will store a query string from search input.
+// TODO 4.2. Create a function, which will take an array of pokemons as a parameter and return a new array of pokemons,
+// TODO      but only those which id, name or type matches the value of string query created in 1.1 (you can use filter function).
+// TODO 4.3. Create a function, which will take care of updating the results. For now, it should take array of all pokemons,
+// TODO      call the function from 1.2. on it and write the result from it into the pokemon array in the state.
+// TODO 4.4. Last function we need to create will be responsible for updating search query in criteria and calling function updating the results.
+// TODO      This function has to be passed to SearchInput component as a prop.
+// TODO 4.5. Add SearchInput component above SearchResults in render function. Remeber to pass it aformentioned function as a prop.
+
   render() {
     return (
       <div>
--- src/SearchInput/SearchInput.js
+++ src/SearchInput/SearchInput.js
@@ -3,6 +3,20 @@ import './SearchInput.css';
 
 class SearchInput extends Component {
 
+// TODO 4.6. We want the render funtion to generate html of this structure:
+// <form className="search">
+//   <div className="search-box">
+//     <input type="text" placeholder="Search" />
+//     <div className="icon" />
+//     <a href="#" className="clear" />
+//   </div>
+// </form>
+// TODO 4.7. Implement this component according to “Single Source of Truth” idea.
+// TODO      You can read about it here (https://reactjs.org/docs/forms.html#controlled-components).
+// TODO 4.8. In the change event handler of the input remember to call passed through props with current input value.
+// TODO 4.9. Add a class of visible to link with clear class when value of the input is not an empty string.
+// TODO      Also, add a click handler to that element that clear the input value and updates the results.
+
 }
 
 export default SearchInput;
