cfc9018b3b8becaa0764fb288ebd1b8338bcc779
Tomasz Gil <tomasz.gil@akai.org.pl>
2018-04-16 01:32:18 +0200

    Step 6 instructions

--- src/Filter/Filter.js
+++ src/Filter/Filter.js
@@ -3,6 +3,32 @@ import './Filter.css';
 
 class Filter extends Component {
 
+// TODO 6.4. Implement Filter component to render html structure:
+// <div className="filter">
+//   <div className="toggle-filter">
+//     <span className="filter-icon" />
+//     <span className="filter-text">Filter Pokedex</span>
+//   </div>
+// TODO      Underneath that, show this part only is a user has clicked on the .toggle-filter element:
+//   <div className="filter-menu">
+//     <div className="types">
+//       <span className="category-title">Pokemon Type</span>
+//       <span className="category-items">
+//       {
+//         pokemonTypes.map(type => <div key={type}
+//                                       data-name={type}
+//                                       style={{
+//                                         backgroundColor: pokemonTypesToColors[type],
+//                                         border: `2px solid ${pokemonTypesToColors[type]}`
+//                                       }}>{type}</div>)
+//       }
+//       </span>
+//     </div>
+//   </div>
+// </div>
+// TODO 6.5. Add a function that will handle opening and closing the menu with filters.
+// TODO 6.6. Add a click event handler to each type pill, which will add or remove corresponding type from the search criteria object and update the results.
+
 }
 
 export default Filter;
