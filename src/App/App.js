184fa77bf226ceefb2bf5eccec2af33b6dc0f81f
Tomasz Gil <tomasz.gil@akai.org.pl>
2018-05-17 00:40:09 +0200

    Update instructions

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 
 // TODO 1.3. Import React.
-// TODO 1.4. Create App class that extends React's Component class.
+// TODO 1.4. Create App class that extends React's Component class. Remember to export the component class
+// TODO      after declaring it to be able to import it and use it in other files (use export default App).
 // TODO 1.5. Component should have a field to store the data fetched from API.
 // TODO 1.6. Initialize the state of the component to have a single variable that indicates whether the data has been already fetched or not.
-// TODO 1.7. Create a function that will fetch a pokemon array from this URL:
+// TODO 1.7. Create a method that will fetch a pokemon array from this URL:
 // TODO      https://api.mlab.com/api/1/databases/pokedex/collections/pokemons?apiKey=RZxUI6ohr3E8hmBGY6HDPlRWpXmVhzgh
-// TODO      Remember to bind this function properly, if you plan to use any component’s properties.
 
 // Hint. You can use the new fetch API. This is how you do that:
 //   fetch(dataURL)
@@ -16,7 +16,8 @@ import './App.css';
 //     })
 //     .catch(err => console.error(err));
 
-// TODO      Single pokemon should have id, name, image link, type and boolean information whether it is collected or not (initialize it with false, we will deal with it later).
-// TODO 1.8. Invoke this function as soon as the component is mounted onto the page.
+// TODO      Single pokemon should have id, name, image link, type (take the first one from array with pokemon's types)
+// TODO      and boolean information whether it is collected or not (initialize it with false, we will deal with it later).
+// TODO 1.8. Invoke this method as soon as the component is mounted onto the page.
 // TODO 1.9. We want this component to render a div with a class of app, inside of which we want to have Logo.
-// TODO      Remember to import the component first.
+// TODO      You can import that component from src/Logo/Logo.js file.
--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
 // TODO Step 1.
-// TODO 1.1. Import App component from src/App.
+// TODO 1.1. Import App component from src/App/App.js file. Remember that importing in JavaScript always works
+// TODO      with relative paths, so pay attention where the file that you're import is located in relation
+// TODO      to the file you are importing. React should throw an error now saying that nothing is to be imported
+// TODO      from this file, once you complete 1.4 this error will go away.
 // TODO 1.2. Render App component inside the element with id of "root".
 
 registerServiceWorker();
