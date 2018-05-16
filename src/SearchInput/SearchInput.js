60f6b7dfdd0be5faa286df39181510f7e706343d
Tomasz Gil <tomasz.gil@akai.org.pl>
2018-05-17 00:52:37 +0200

    Update instructions

--- src/SearchInput/SearchInput.js
+++ src/SearchInput/SearchInput.js
@@ -3,7 +3,7 @@ import './SearchInput.css';
 
 class SearchInput extends Component {
 
-// TODO 4.6. We want the render funtion to generate html of this structure:
+// TODO 4.6. We want the render function to generate html of this structure:
 // <form className="search">
 //   <div className="search-box">
 //     <input type="text" placeholder="Search" />
@@ -13,9 +13,9 @@ class SearchInput extends Component {
 // </form>
 // TODO 4.7. Implement this component according to “Single Source of Truth” idea.
 // TODO      You can read about it here (https://reactjs.org/docs/forms.html#controlled-components).
-// TODO 4.8. In the change event handler of the input remember to call passed through props with current input value.
+// TODO 4.8. In the change event handler of the input (onChange attribute) remember to call passed through props with current input value.
 // TODO 4.9. Add a class of visible to link with clear class when value of the input is not an empty string.
-// TODO      Also, add a click handler to that element that clear the input value and updates the results.
+// TODO      Also, add a click handler to that element (onClick attribute) that clear the input value and updates the results.
 
 }
 
