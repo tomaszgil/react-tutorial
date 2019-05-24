dee9e17f3bc5f479320933cc47aa11fe573714dd
plutasnyy <kamil.plucinski97@gmail.com>
2019-05-24 14:31:55 +0200

    fixed 6 step

--- src/CustomCheckbox/CustomCheckbox.js
+++ src/CustomCheckbox/CustomCheckbox.js
@@ -1,53 +1,27 @@
 import React, {Component} from 'react';
-import Logo from '../Logo/Logo';
-import Search from '../Search/Search';
-import '../App/App.css';
+import './CustomCheckbox.css';
 
-class App extends Component {
+class CustomCheckbox extends Component {
     constructor(props) {
         super(props);
 
-        this.apiAccessKey = "RZxUI6ohr3E8hmBGY6HDPlRWpXmVhzgh";
-        this.allPokemons = [];
-
-        this.state = {
-            isFetched: false
-        };
-
-        this.fetchPokemons = this.fetchPokemons.bind(this);
+        this.checkedClass = 'checked';
+        this.getClassName = this.getClassName.bind(this);
     }
 
-    componentDidMount() {
-        this.fetchPokemons();
-    }
-
-    fetchPokemons() {
-        fetch(`https://api.mlab.com/api/1/databases/pokedex/collections/pokemons?apiKey=${this.apiAccessKey}`)
-            .then(blob => blob.json())
-            .then(data => {
-                this.allPokemons = data.map(element => ({
-                    name: element.name,
-                    id: element.id,
-                    img: element.image,
-                    type: element.types[0],
-                    collected: false
-                }));
-
-                this.setState({
-                    isFetched: true,
-                });
-            })
-            .catch(err => console.error(err));
+    getClassName() {
+        return this.props.checked ? `custom-checkbox ${this.checkedClass}` : 'custom-checkbox';
     }
 
     render() {
         return (
-            <div className="app">
-                <Logo/>
-                <Search isFetched={this.state.isFetched} pokemons={this.allPokemons}/>
+            <div className={this.getClassName()} onClick={() => this.props.onClick(this.props.id)}>
+                <input type="checkbox" defaultChecked={this.props.checked}/>
+                <span className="radio"/>
+                <label>{this.props.label}</label>
             </div>
         );
     }
 }
 
-export default App;
+export default CustomCheckbox;
--- src/Search/Search.js
+++ src/Search/Search.js
@@ -1,8 +1,10 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Menu from '../Menu/Menu';
 import SearchInput from '../SearchInput/SearchInput';
 import SearchResults from '../SearchResults/SearchResults';
 import filters from '../_utils/Filters';
+import Filter from "../Filter/Filter";
+import {pokemonTypes} from "../_utils/Pokemon";
 
 class Search extends Component {
     constructor(props) {
@@ -21,20 +23,22 @@ class Search extends Component {
             },
             filter: {
                 collected: filters.SHOW_ALL
-            }
+            },
+            chosenTypes: pokemonTypes
         };
 
         this.handleSorting = this.handleSorting.bind(this);
         this.handleSearchQuery = this.handleSearchQuery.bind(this);
         this.handleFilterCollected = this.handleFilterCollected.bind(this);
         this.handlePokemonStateChange = this.handlePokemonStateChange.bind(this);
+        this.handlePokemonTypesChange = this.handlePokemonTypesChange.bind(this)
         this.sort = this.sort.bind(this);
         this.processSearchQuery = this.processSearchQuery.bind(this);
         this.applyFilters = this.applyFilters.bind(this);
         this.updateResults = this.updateResults.bind(this);
     }
 
-    componentWillReceiveProps(nextProps){
+    componentWillReceiveProps(nextProps) {
         if (nextProps.isFetched) {
             this.allPokemons = nextProps.pokemons;
             this.updateResults();
@@ -57,45 +61,40 @@ class Search extends Component {
     }
 
     handleSorting(key, direction) {
+        console.log(key)
+        console.log(direction)
         this.criteria.sort.key = key;
         this.criteria.sort.direction = direction;
         this.updateResults();
     }
 
-    sort(arr) {
-        let multiplier = 1;
+    sort(pokemons) {
         const key = this.criteria.sort.key;
+        let isAsc = this.criteria.sort.direction === "asc";
 
-        switch(this.criteria.sort.direction) {
-            case 'ascending': multiplier = 1; break;
-            case 'descending': multiplier = -1; break;
-            default: break;
-        }
-
-        return arr.sort((a, b) => {
-            if (a[key] < b[key]) return -1 * multiplier;
-            if (a[key] === b[key]) return 0;
-            if (a[key] > b[key]) return multiplier;
-
+        return pokemons.sort((a, b) => {
+            if (a[key] < b[key]) return isAsc ? -1 : 1;
+            if (a[key] > b[key]) return isAsc ? 1 : -1;
             return 0;
         });
     }
 
-// TODO Step 6.
-// TODO 6.1. To Search component add a new field in search criteria which will store array of currently chosen pokemon types.
-// TODO      You can import types from src/_utils/Filters.js file.
-// TODO 6.2. Add a method that will take care of updating this field.
-// TODO 6.3. Add or modify a function to filter given array upon selected types.
-
     applyFilters(arr) {
         let result = [];
-        switch(this.criteria.filter.collected) {
-            case filters.SHOW_ALL: result = arr; break;
-            case filters.ONLY_COLLECTED: result = arr.filter(el => el.collected); break;
-            case filters.NOT_COLLECTED: result = arr.filter(el => !el.collected); break;
-            default: result = arr;
+        switch (this.criteria.filter.collected) {
+            case filters.SHOW_ALL:
+                result = arr;
+                break;
+            case filters.ONLY_COLLECTED:
+                result = arr.filter(el => el.collected);
+                break;
+            case filters.NOT_COLLECTED:
+                result = arr.filter(el => !el.collected);
+                break;
+            default:
+                result = arr;
         }
-
+        result = result.filter(p => this.criteria.chosenTypes.includes(p.type));
         return result;
     }
 
@@ -122,12 +121,20 @@ class Search extends Component {
         });
     }
 
+    handlePokemonTypesChange(types) {
+        console.log(types)
+        this.criteria.chosenTypes = types;
+        this.updateResults();
+    }
+
     render() {
         return (
             <div>
-                <SearchInput onChange={this.handleSearchQuery} />
-                <Menu onFilterChange={this.handleFilterCollected} onSortChange={this.handleSorting} />
-                <SearchResults pokemons={this.state.pokemons} isFetched={this.props.isFetched} onPokemonCheck={this.handlePokemonStateChange} />
+                <SearchInput onChange={this.handleSearchQuery}/>
+                <Menu onFilterChange={this.handleFilterCollected} onSortChange={this.handleSorting}/>
+                <Filter onChange={(types) => this.handlePokemonTypesChange(types)}/>
+                <SearchResults pokemons={this.state.pokemons} isFetched={this.props.isFetched}
+                               onPokemonCheck={this.handlePokemonStateChange}/>
             </div>
         );
     }
